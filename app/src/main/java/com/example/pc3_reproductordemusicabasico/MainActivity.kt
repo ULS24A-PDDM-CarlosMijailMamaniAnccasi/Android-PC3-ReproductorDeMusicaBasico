aabad2be3ff72965bb761d9ecdcd22fa4d673edf
Carlos Mijail Mamani Anccasi <cmamania@ulasalle.edu.pe>
2024-04-09 12:51:17 -0500

    add comentario introductorio

--- app/src/main/java/com/example/pc3_reproductordemusicabasico/MainActivity.kt
+++ app/src/main/java/com/example/pc3_reproductordemusicabasico/MainActivity.kt
@@ -3,6 +3,14 @@ package com.example.pc3_reproductordemusicabasico
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * @problemDescription Se solicita, crear un reproductor de musica basico que contenga:
+ * reproducir, pausar y detener la musica local.
+ * @author Carlos Mijail Mamani Anccasi
+ * @creationDate 09/04/24
+ * @lastModification 09/04/24
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
